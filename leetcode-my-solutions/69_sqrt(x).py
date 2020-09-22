cf4d294a02ace1bfa8dcdb2fb5823669c741e47f
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2020-09-21 21:34:23 -0500

    Update 69_sqrt(x).py

--- leetcode-my-solutions/69_sqrt(x).py
+++ leetcode-my-solutions/69_sqrt(x).py
@@ -1,12 +1,9 @@
 """
-Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+Author: Sikder Tahsin Al-Amin
+Problem: Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
 Input: 8
 Output: 2
 """
 def mySqrt(x):
     sqrt = x ** (0.5)
     return int(sqrt)    
-
-
-x = 8
-print("output=",mySqrt(x))
