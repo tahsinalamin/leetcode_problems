436a9c0d064bbf21845bc4dd7d265497a03bde3a
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2020-09-21 21:33:52 -0500

    Update 144_binary_tree_preorder.py

--- leetcode-my-solutions/144_binary_tree_preorder.py
+++ leetcode-my-solutions/144_binary_tree_preorder.py
@@ -1,5 +1,6 @@
 """
-Given a binary tree, return the preorder traversal of its nodes' values.
+Author: Sikder Tahsin Al-Amin
+Problem: Given a binary tree, return the preorder traversal of its nodes' values.
 Input: [1,null,2,3]
 Output: [1,2,3
 """
