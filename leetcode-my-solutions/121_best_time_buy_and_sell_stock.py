5c053f6396003ac7a7ca51755627bd87835c8262
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2020-09-21 21:36:07 -0500

    Update 121_best_time_buy_and_sell_stock.py

--- leetcode-my-solutions/121_best_time_buy_and_sell_stock.py
+++ leetcode-my-solutions/121_best_time_buy_and_sell_stock.py
@@ -1,5 +1,6 @@
 """
-Say you have an array for which the ith element is the price of a given stock on day i.
+Author: Sikder Tahsin Al-Amin
+Problem:  Say you have an array for which the ith element is the price of a given stock on day i.
 If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 Input: [7,1,5,3,6,4]
 Output: 5
@@ -14,6 +15,3 @@ def maxProfit(prices):
             profit = prices[i] - min
     print("mim=",min)
     return profit
-
-prices = [7,1,5,3,6,4]
-print("output:",maxProfit(prices))
