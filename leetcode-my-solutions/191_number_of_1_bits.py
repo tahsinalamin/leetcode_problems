0a8d4c3a00dc53e7b50e04587885ef1bea75afce
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2020-09-28 12:41:00 -0500

    Update 191_number_of_1_bits.py

--- leetcode-my-solutions/191_number_of_1_bits.py
+++ leetcode-my-solutions/191_number_of_1_bits.py
@@ -1,4 +1,6 @@
 """
+Author: Sikder Tahsin Al Amin
+Problem: 
 Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight).
 Input: 00000000000000000000000000001011
 Output: 3
@@ -16,6 +18,3 @@ def hammingWeight(n):
             if i=='1':
                 count = count +1
         return count
-        
-Input= 11111111111111111111111111111101
-print("output: ", hammingWeight(Input))
