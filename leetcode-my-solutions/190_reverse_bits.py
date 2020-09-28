dbc5dfb2b75d8c683d0cb8935a081c47a2d4b7c1
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2020-09-28 12:39:53 -0500

    Update 190_reverse_bits.py

--- leetcode-my-solutions/190_reverse_bits.py
+++ leetcode-my-solutions/190_reverse_bits.py
@@ -13,33 +13,3 @@ def reverseBits(n):
     for i in n:
         s=s+i
     return s
-
-
-
-
-n = '00000010100101000001111010011100'
-print("output:",reverseBits(n))
-
-###cracking coding interview 5.1
-def insertion(N,M,i,j):
-    N = list(str(N))
-    N.reverse()
-    M = list(str(M))
-    M.reverse()
-    m_index = 0
-    for index in range(0,len(N)):
-        if index <= j-1 and index >=i-1:
-            N[index] = M[m_index]
-            m_index = m_index - 1
-        print(index, N[index])
-    s=""
-    for index in N:
-        s =  index + s
-    return s
-
-
-N = 1000000000
-M = 10011
-i = 2
-j = 6
-print("output:",insertion(N,M,i,j))
